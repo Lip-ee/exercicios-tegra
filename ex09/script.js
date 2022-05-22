2713644e0c99e8c8418633d639ce350cad19b25f
Filipe Campos <63020408+Lip-ee@users.noreply.github.com>
2022-05-21 23:51:44 -0300

    updating .js file ex09

--- ex09/script.js
+++ ex09/script.js
@@ -13,14 +13,3 @@ function calcular(){
     ${h}:${m}:${s}
     `;
 }
-
-// function resolverEx9() {
-//     let duracao = parseInt(document.getElementById('inputDuracao').value);
-    
-//     let horas = Math.floor(duracao / 3600);
-//     let minutos = Math.floor(duracao % 3600 / 60);
-//     let segundos = Math.floor(duracao % 3600 % 60);
-    
-//     let textArea = document.getElementById('outputArea');
-//     textArea.textContent += `Digite a duração em segundos: ${duracao}\n${horas}:${minutos}:${segundos}\n\n`
-// }
