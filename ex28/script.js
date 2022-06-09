326b9ffc18dde6af7c6cc8265957fe68810250f8
Filipe Campos <lip3.campos@gmail.com>
2022-06-09 01:17:07 -0300

    ex25 finished and ex26 to ex30 solved

--- ex28/script.js
+++ ex28/script.js
@@ -1,10 +1,20 @@
 function calcular(){
 
     // atribuindo variáveis e resolvendo a lógica
-    let variavel = document.getElementById("varID").value;
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+    let num3 = parseInt(document.getElementById("num3").value);
+    let num4 = parseInt(document.getElementById("num4").value);
+    let num5 = parseInt(document.getElementById("num5").value);
+
+    let nums = [num1, num2, num3, num4, num5];
+
+    let maior = Math.max(...nums),
+        menor = Math.min(...nums);
     
     // output
     document.getElementById("output").innerHTML = `
-    input_text = <span><b>${variavel}</b></span> <br><br>
+    Valores: <span><b>${nums.join(", ")}</b></span> <br><br>
+    Saída: <b>${menor} e ${maior}</b>
     `;
 }
--- ex30/script.js
+++ ex30/script.js
@@ -1,10 +1,26 @@
 function calcular(){
 
     // atribuindo variáveis e resolvendo a lógica
-    let variavel = document.getElementById("varID").value;
+    let numero = parseInt(document.getElementById('numero').value);
+    
+    let auxiliar1 = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'];
+    let auxiliar2 = ['vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'];
+    let extenso;
+    
+    if(numero > 99 || numero < 0){
+        extenso = `${numero} não está entre 0 e 99. Por favor, tente novamente.`
+    }else if(numero < 20){
+        extenso = auxiliar1[numero];
+    }else{
+        let dec = Math.floor(numero / 10);
+        let unit = numero % 10;
+        
+        extenso = auxiliar2[dec - 2] + (unit > 0 ? ` e ${auxiliar1[unit]}` : '');
+    }
     
     // output
     document.getElementById("output").innerHTML = `
-    input_text = <span><b>${variavel}</b></span> <br><br>
+    Número: <span><b>${numero}</b></span> <br><br>
+    <b>Saída: ${extenso}</b>
     `;
 }
